fe98405a7ea814da84035f07d3c07b65a67b4d60
VidyanandSawai17 <vidyanandsawai17@gmail.com>
2023-01-23 22:32:41 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            emp emp1 = new emp("Ram",15000);
+            emp emp2 = new emp("shyam", 20000);
+
+            Console.WriteLine(emp1.Print());
+            Console.WriteLine(emp2.Print());
+
+            Console.WriteLine($"\n \n Total objects of class {emp.DisplayCount()}"); 
+            
+            // static method is called using class name
+
+        }
+
+    }
+}
---
+++ emp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8
+{
+    public class emp
+    {
+        private int id;
+        private string empname;
+        private double salary;
+        
+        private static int count = 5; // static variable or class variable 
+        public emp(string empname, double salary)
+        {
+            this.empname = empname;
+            this.salary = salary;
+           
+            
+            id = count;
+            count++;
+        }
+
+        public static int DisplayCount() // static method or class method   
+
+            /*Static method is also called as  class method, which is used to display value of static variable */
+
+
+        {
+            return count;
+        }
+
+        
+
+        public string Print() // concret or instance method
+        {
+            return $"\n Id : {id} ,\n Name : {empname},\n salary : {salary} ";
+        }
+
+
+    }
+}
